ca68d0388c0c58e5edaafc3524faedab9c42e1e0
sanlorenita-18 <sanlorenita-18@hotmail.com>
2023-01-27 16:31:02 -0500

    adelanto de testing#6

--- test/firebase.spec.js
+++ test/firebase.spec.js
@@ -2,7 +2,7 @@
 // eslint-disable-next-line import/no-unresolved
 import { exitConsult } from '../src/lib/auth.js';
 // eslint-disable-next-line import/no-unresolved
- import { auth, signInWithEmailAndPassword, createUserWithEmailAndPassword }   from '../src/lib/firebase.js';
+ import { auth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signInWithPopup }   from '../src/lib/firebase.js';
 
  //primer mock de autenticacion
 jest.mock('../src/lib/firebase.js', () => ({  //aqui se accede a la libreria "JEST"
@@ -20,8 +20,16 @@ signInWithEmailAndPassword : jest.fn((email, password) =>{
 }),
 //tercer Mock "createUserWithEmailAndPassword"
 createUserWithEmailAndPassword : jest.fn((email, password)=>{
-  if (condition) {
-    
+  if(email === 'correonoexistente@gmail.com'){
+    throw new Error('Correo inválido');
+  }if(password === '123'){
+    throw new Error('Contraseña inválida'); //lanzamos el error
+  }
+}),
+//cuarto Mock "signInWithPopup"
+signInWithPopup : jest.fn((email) =>{
+  if(email === '2020sandrarios@gamil.com'){
+    return('Correo válido');
   }
 })
 }))
@@ -35,7 +43,7 @@ describe('Se testea la función Auth', () =>{
     }catch(error){
       expect(error.message).toBe('Correo inválido')
     }
-  })
+  });
   it('signInWithEmailAndPassword debe devolver un password incorrecto', async() =>{
     try {
       const email = 'correonoexistente@gmail.com';
@@ -44,7 +52,42 @@ describe('Se testea la función Auth', () =>{
     } catch (error) {
       expect(error.message).toBe('Contraseña inválida')
     }
-  })
+  });
+  it('createUserWithEmailAndPassword debe dar email incorrecto', async() =>{
+    try {
+      const email = 'correonoexistente@gmail.com';
+      const password = '123';
+      await createUserWithEmailAndPassword(auth, email, password);
+    } catch (error) {
+      expect(error.message).toBe('Correo inválido')
+    }
+  });
+  it('createUserWithEmailAndPassword debe dar email incorrecto', async() =>{
+    try {
+      const email = 'correonoexistente@gmail.com';
+      const password = '123';
+      await createUserWithEmailAndPassword(auth, email, password);
+    } catch (error) {
+      expect(error.message).toBe('Correo inválido')
+    }
+  });
+  it('createUserWithEmailAndPassword debe dar contraseña incorrecta', async() =>{
+    try {
+      const email = 'correonoexistente@gmail.com';
+      const password = '123';
+      await createUserWithEmailAndPassword(auth, email, password);
+    } catch (error) {
+      expect(error.message).toBe('Contraseña incorrecta')
+    }
+});
+it('signInWithPopup devuelve un correo válido', async() =>{
+  try {
+    const email = '2020sandrarios@gamil.com';
+    await signInWithPopup(email)
+  } catch (error) {
+    expect(error.message).toBe('Correo válido');
+  }
+})
 })
 
 

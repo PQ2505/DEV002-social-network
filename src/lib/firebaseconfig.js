8346b17129373ecb3a49d9e44504f3e3bd7362fa
Paola Quiroga <paolaquirogap10@gmail.com>
2023-02-06 15:20:42 -0500

    se cambia dominio

--- src/lib/firebaseconfig.js
+++ src/lib/firebaseconfig.js
@@ -2,7 +2,7 @@
 // Configuración Firebase al inicializar sus servicios
 export default {
   apiKey: 'AIzaSyCxLhAk9jOlsC43X5vqcFhw-eBCbrELaI4',
-  authDomain: 'https://glittering-bonbon-7f7508.netlify.app/',
+  authDomain: 'newme-13925.firebaseapp.com',
   projectId: 'newme-13925',
   storageBucket: 'newme-13925.appspot.com',
   messagingSenderId: '214920308604',
